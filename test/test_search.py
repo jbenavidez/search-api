26b9508694f8f74d5f8a6503ba0359006d7a634c
JBenavidez <jonathanbenavidez@nypl.org>
2023-04-18 01:13:33 -0400

    fix lint issues

--- test/test_search.py
+++ test/test_search.py
@@ -3,10 +3,10 @@
 import pytest
 from rest_framework.test import APIClient, APIRequestFactory
 from django.core.management import call_command
-from django.urls import reverse
 
 client = APIClient()
-factory  =APIRequestFactory()
+factory = APIRequestFactory()
+
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
@@ -14,18 +14,22 @@ def django_db_setup(django_db_setup, django_db_blocker):
         # plant the seed
         call_command('seed')
 
- 
+
 def test_search_only_q(db, django_db_setup):
-    """this test is to test if the search endpoint returns 200 and the data is correct"""
+    """This test is to test if the search endpoint
+    returns 200 and the data is correct
+    """
     # Arrange
     res = client.get('/api/search?q=Mathematics')
     # Act
     data = res.json()
     assert isinstance(data['data'], list)
-    assert 200==  res.status_code
+    assert 200 == res.status_code
+
 
 def test_search_q_plus_include(db, django_db_setup):
-    """this test is to test if search endpoint return only data with include keyword"""
+    """This test is to test if search endpoint
+    return only data with include keyword"""
 
     # Arrange
     res = client.get('/api/search?include=Gutmann%20LLC&q=Mathematics')
@@ -36,9 +40,11 @@ def test_search_q_plus_include(db, django_db_setup):
 
 
 def test_check_keys(db, django_db_setup):
-    """this test is to test if the search endpoint return a valid list of dicts"""
+    """ This test is to test if the search endpoint
+        return a valid list of dicts
+    """
     # Arrange
-    test_obj ={
+    test_obj = {
                 'id': 60,
                 'first_name': 'Tomlin',
                 'last_name': 'Freddi',
